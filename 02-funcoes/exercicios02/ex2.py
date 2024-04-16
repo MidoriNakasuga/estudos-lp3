00752b5dc4d8715cc905fc8e05eb23552e13ce87
MidoriNakasuga <162324424+MidoriNakasuga@users.noreply.github.com>
2024-04-16 17:13:33 -0300

    Update ex02.py

--- 02-funcoes/exercicios02/ex2.py
+++ 02-funcoes/exercicios02/ex2.py
@@ -1,26 +1,10 @@
 #Ex02 - Tabuada de Um Número: Solicite ao usuário um número e exiba a tabuada desse número de 1 a 10.
+def tabuada():
+ num=int(input('Digite um número '))
+ tab=1
+ while tab != 11:
+  res= tab*num
+  print(f'{num} x {tab} = {res}')
+  tab=tab+1
 
-numero = int(input('Digite um número para visualiazar a tabuada'))
-
-
-#res2 = numero * 2
-#res2 = numero * 3
-#res2 = numero * 4
-#res2 = numero * 5
-#res2 = numero * 6
-#res2 = numero * 7
-#res2 = numero * 8
-#res2 = numero * 9
-##res2 = numero * 10
-#print(f'{numero} x 1 = {numero}')
-#print(f'{numero} x 2 = {res2}')
-#print(f'{numero} x 3 = {res3}')
-#print(f'{numero} x 4 = {res4}')
-##print(f'{numero} x 5 = {res5}')
-#print(f'{numero} x 6 = {res6}')
-##print(f'{numero} x 7 = {res7}')
-#print(f'{numero} x 2 = {res8}')
-#print(f'{numero} x 2 = {res9}')
-#print(f'{numero} x 2 = {res10}')
-
-
+tabuada()
