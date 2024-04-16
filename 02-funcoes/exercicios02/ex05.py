6420b6f9022c9633ca35dae177e46d39640fdc27
MidoriNakasuga <162324424+MidoriNakasuga@users.noreply.github.com>
2024-04-16 17:18:07 -0300

    Update ex05.py

--- 02-funcoes/exercicios02/ex05.py
+++ 02-funcoes/exercicios02/ex05.py
@@ -1,7 +1,6 @@
 #Ex05 - Verificador de Palíndromos: Solicite ao usuário que digite uma palavra ou frase e verifique se é um palíndromo (ou seja, pode ser lida de frente para trás e de trás para frente da mesma forma).
 def palindromo():
     texto = input("Digite uma palavra ou frase: ")
-    texto 
     texto = texto.replace(" ", "")
     if texto == texto[::-1]: 
         print('É um palíndromo')
